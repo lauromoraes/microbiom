5327f170cd9a731d5136fff3a787ffa07a5d153e
lauromoraes <lauromoraes@ufop.edu.br>
2022-11-18 18:11:50 +0000

    add step: picrust2-analysis

--- pipeline.sh
+++ pipeline.sh
@@ -20,7 +20,8 @@ STEPS=(
 	"step-rarefaction-analysis"
 	"step-metataxonomy"
 	"step-diversity-analysis"
-    "step-abundance-analysis"
+	"step-abundance-analysis"
+	"step-picrust2-analysis"
 	);
 
 STEPSDIR="nb-templates"
