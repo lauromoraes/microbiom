7f0bf5e533eeefc5ef9a71f49148eb81e207ce62
lauromoraes <lauromoraes@ufop.edu.com>
2024-02-19 20:32:28 +0000

    refactor: prepare data step

--- nb-templates/utils.py
+++ nb-templates/utils.py
@@ -16,3 +16,38 @@ def filter_samples(metadata, tabs, reps)
         table=tabs,
     ).filtered_data
     return tabs, reps
+
+
+
+def get_direction_info(manifest_file_path):
+    """Process and gets information about fastq directions using manifest file
+    
+    Example:
+        d_type, v_type, direction = get_direction_info(manifest_file)
+        print('\n'.join([d_type, v_type, direction]))
+
+    Parameters:
+    manifest_file_path (str): manifest file with all sample input paths
+
+    Returns:
+    str: QIIME2 input data type
+    str: QIIME2 Manifest file type
+    str: Direction type
+
+    """
+    d_type, v_type, direction = None, None, None
+    manifest_df = pd.read_csv(manifest_file)
+    n_directions = len(manifest_df['direction'].unique())
+    if n_directions == 1:
+        d_type = 'SampleData[SequencesWithQuality]'
+        v_type = 'SingleEndFastqManifestPhred33'
+        direction = 'single'
+    elif n_directions == 2:
+        d_type = 'SampleData[PairedEndSequencesWithQuality]'
+        v_type = 'PairedEndFastqManifestPhred33'
+        direction = 'paired'
+    else:
+        print(f'ERROR: invalid number of directions {n_directions}')
+    return d_type, v_type, direction
+
+
