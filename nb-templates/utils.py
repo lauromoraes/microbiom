4a3d241e355cecd462e2ad71dd99b51549452afb
Lauro Moraes <lauromoraes@ufop.edu.br>
2026-09-01 19:00:42 -0300

    Fix case sensitivity in rank prefix check

--- nb-templates/utils.py
+++ nb-templates/utils.py
@@ -119,7 +119,7 @@ def get_deepest_taxonomic_level(taxonomy) -> int:
             rank = rank.strip()
 
             for prefix, level in prefixes.items():
-                if lower(rank).startswith(prefix):
+                if rank.lower().startswith(prefix):
                     # Ignore empty classifications such as g__ or s__
                     value = rank[len(prefix):].strip()
 
